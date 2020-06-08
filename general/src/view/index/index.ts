aa95da06708314b9200c3cfc14619c0d6dd13e43
tangjievic <981955667@qq.com>
2020-06-08 19:58:59 +0800

    feat(视图层): 首页pc视图修改

--- general/src/view/index/index.ts
+++ general/src/view/index/index.ts
@@ -1,5 +1,8 @@
 import './index.less';
 import '../page';//引入页面公共ts方法模块
-import '../../components/plugin/sliderbanner/index.js'
+import '../../components/plugin/sliderbanner/index.js';
+import '../../components/plugin/showreel/index.js';
+import '../../components/elements/artlist/index';
+
 let a = 10;
 console.log(a)
--- general/src/view/page.ts
+++ general/src/view/page.ts
@@ -7,6 +7,14 @@ import '../components/wetui/base/base.css';
 import '../components/wetui/base/animation.css';
 import '../components/wetui/input/index.css';
 import '../components/wetui/btn/index.css';
+import '../components/elements/footer/index.less';
+import '../components/elements/leftele/leftele.less';
+
+import '../components/elements/righttool/index';
+import '../components/elements/bubbleline/index.css';
+import '../components/elements/adlist/index.less';
+import '../components/elements/bgdiv/index.less';
+
 import '../libs/rem.js';
 
 import jq from 'jquery'
