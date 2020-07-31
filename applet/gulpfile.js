6c09fc21df21a33e8439ec9c6d0de0352d6ff628
v_jeetang <v_jeetang@tencent.com>
2020-07-31 21:06:28 +0800

    初步规划日期选择器

--- applet/gulpfile.js
+++ applet/gulpfile.js
@@ -10,71 +10,110 @@ const del = require('del');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
+
+
+// 项目路径
 const option = {
-    base:'src',
-    allowEmpty:true
+    base: 'src',
+    allowEmpty: true,
 };
-const buildPath = 'dist';
-const dist = `${__dirname}/${buildPath}`;
-const copyPath = ['src/**/!(_)*.*', '!src/**/*.less','!src/**/*.ts'];
+// 此处为输出目录
+const builtPath = 'dist';
+
+const dist = `${__dirname}/${builtPath}`;
+const copyPath = ['src/**/!(_)*.*', '!src/**/*.less', '!src/**/*.ts'];
 const lessPath = ['src/**/*.less', 'src/app.less'];
 const watchLessPath = ['src/**/*.less', 'src/css/**/*.less', 'src/app.less'];
 const tsPath = ['src/**/*.ts', 'src/app.ts'];
 
-//清空目录dist
-function cleanDist(cb) {
-    gulp.src(dist,{
-        allowEmpty:true,
-        read: false,
-        //since: gulp.lastRun(cleanDist)
-    }).pipe(clear());
-    cb();
-}
-//复制文件除开_的文件和less文件
-function copyFile(cb){
-    gulp.src(copyPath,option,{
-        //since: gulp.lastRun(copyFile)
-    }).pipe(changed(dist)).pipe(gulp.dest(dist));
-    cb();
-}
-//复制改动的文件不包含less
-function copyChange(cb){
-    gulp.src(copyPath,option)
-    .pipe(changed(dist))
-    .pipe(gulp.dest(dist))
-    cb()
-}
+// 清空目录
+gulp.task('clear', () => gulp.src(dist, { allowEmpty: true }).pipe(clear()));
 
-//编译less
-function lessComplie(cb){
-    gulp.src(lessPath,option,{since: gulp.lastRun(lessComplie)})
-    .pipe(less().on('error',function(e){
+// 复制不包含less和图片的文件
+gulp.task('copy', () => gulp.src(copyPath, option).pipe(gulp.dest(dist)));
+// 复制不包含less和图片的文件(只改动有变动的文件）
+gulp.task('copyChange', () => gulp
+    .src(copyPath, option)
+    .pipe(changed(dist))
+    .pipe(gulp.dest(dist)));
+
+// 编译less
+gulp.task('less', () => gulp
+    .src(lessPath, option)
+    .pipe(less().on('error', function (e) {
         console.error(e.message);
         this.emit('end');
-    })).pipe(postcss([autoprefixer]))
-    .pipe(rename((path)=>{
-        path.extname = '.wxss'
-    })).pipe(gulp.dest(dist));
-    
-    cb();
-}
-//编译ts
-function tsCompile(cb){
-    gulp.src(['./typings/**/*.ts'].concat(tsPath),option,{since: gulp.lastRun(tsCompile)}).pipe(sourcemaps.init()).pipe(tsProject())
-    .js.pipe(sourcemaps.write()).pipe(gulp.dest(buildPath));
-    cb()
-}
-//监听
-function watchEvent(cb){
-    gulp.watch(tsPath,gulp.series(tsCompile));
-    const watcher = gulp.watch(copyPath,gulp.series(copyChange));
-    gulp.watch(watchLessPath,gulp.series(lessComplie));
-    watcher.on('unlink',(filepath)=>{
-        const filePathFromSrc = path.relative(path.resolve('src'),filepath);
+    }))
+    .pipe(postcss([autoprefixer]))
+    .pipe(rename((path) => {
+        path.extname = '.wxss';
+    }))
+    .pipe(gulp.dest(dist)));
+// 编译less(只改动有变动的文件）
+gulp.task('lessChange', () => gulp
+    .src(lessPath, option)
+    .pipe(changed(dist))
+    .pipe(less().on('error', function (e) {
+        console.error(e.message);
+        this.emit('end');
+    }))
+    .pipe(postcss([autoprefixer]))
+    .pipe(rename((path) => {
+        path.extname = '.wxss';
+    }))
+    .pipe(gulp.dest(dist)));
+
+// 编译
+gulp.task('tsCompile', () => tsProject
+    .src()
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .js.pipe(sourcemaps.write())
+    .pipe(gulp.dest(builtPath)));
+
+// 监听
+gulp.task('watch', () => {
+    gulp.watch(tsPath, gulp.series('tsCompile'));
+    const watcher = gulp.watch(copyPath, gulp.series('copyChange'));
+    gulp.watch(watchLessPath, gulp.series('less')); // Change
+    watcher.on('unlink', (filepath) => {
+        const filePathFromSrc = path.relative(path.resolve('src'), filepath);
+        // Concatenating the 'build' absolute path used by gulp.dest in the scripts task
         const destFilePath = path.resolve(builtPath, filePathFromSrc);
+        // console.log({filepath, filePathFromSrc, destFilePath})
         del.sync(destFilePath);
-    })
-    cb()
-}
-exports.default = gulp.series(gulp.parallel(copyFile,lessComplie,tsCompile),watchEvent);
-exports.build = gulp.series(cleanDist,copyFile,lessComplie,tsCompile);
+    });
+});
+
+// 开发并监听
+gulp.task(
+    'default',
+    gulp.series(
+        // sync
+        gulp.parallel(
+            'copy',
+            // "copyNodeModules",
+            // "generatePackageJson",
+            'less',
+            'tsCompile',
+        ),
+        'watch',
+    ),
+);
+
+// 上线
+gulp.task(
+    'build',
+    gulp.series(
+        // sync
+        'clear',
+        gulp.parallel(
+            // async
+            'copy',
+            // "copyNodeModules",
+            // "generatePackageJson",
+            'less',
+            'tsCompile',
+        ),
+    ),
+);
